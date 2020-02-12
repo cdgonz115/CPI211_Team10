6656acd26123320354e96a8506ea29db09fdad02
propredator23 <christiandash@hotmail.com>
2020-02-12 15:00:03 -0700

    minor changes
    reorganized some folders in unity and started messing with the gravity

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -13,6 +13,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
     public class FirstPersonController : MonoBehaviour
     {
         [SerializeField] public bool m_IsWalking;
+        [SerializeField] public bool gravity=false;
         [SerializeField] public float m_WalkSpeed;
         [SerializeField] private float m_RunSpeed;
         [SerializeField] [Range(0f, 1f)] private float m_RunstepLenghten;
@@ -66,6 +67,8 @@ namespace UnityStandardAssets.Characters.FirstPerson
         private void Update()
         {
             RotateView();
+            if (!gravity && Input.GetKeyDown(KeyCode.E)) gravity = true;
+            else gravity = false;
             // the jump state needs to read here to make sure it is not missed
             if (!m_Jump)
             {
@@ -121,7 +124,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
                 if (m_Jump)
                 {
-                    m_MoveDir.y = m_JumpSpeed;
+                    m_MoveDir.y = (gravity)?-m_JumpSpeed:m_JumpSpeed;
                     PlayJumpSound();
                     m_Jump = false;
                     m_Jumping = true;
@@ -129,7 +132,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
             }
             else
             {
-                m_MoveDir += Physics.gravity * m_GravityMultiplier * Time.fixedDeltaTime;
+                m_MoveDir += Physics.gravity * ((gravity) ? -m_GravityMultiplier : m_GravityMultiplier) * Time.fixedDeltaTime;
             }
             m_CollisionFlags = m_CharacterController.Move(m_MoveDir * Time.fixedDeltaTime);
 
