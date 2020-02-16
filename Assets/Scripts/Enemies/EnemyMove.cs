e4e6329a27e54c786eb8c93bc1559057d334fb4b
bchan7 <bchan7@asu.edu>
2020-02-15 17:14:05 -0700

    Enemy Spawner
    Added Enemy Spawner
    Varied movement speed for Enemies

---
+++ Assets/Scripts/Enemies/EnemyMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class EnemyMove : MonoBehaviour
+{
+    public float moveSpeed;
+    public Transform target;
+    public Transform enemies;
+    Rigidbody rbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = Random.Range(.05f, 4f);
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        rbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, target.position);
+        if (distance >= 2)
+        {
+            enemies.position = Vector3.MoveTowards(enemies.position, target.position, Time.deltaTime * moveSpeed);
+        }
+        
+    }
+}
