5f3cc4beb2e5951d640751e2417cb2d646c5adce
propredator23 <christiandash@hotmail.com>
2020-02-15 17:29:11 -0700

    Update
    Imported Assets and started resizing

---
+++ Assets/Scripts/Enemies/MoveEnemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEnemy : MonoBehaviour
+{
+    public float moveSpeed;
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = Random.Range(5f, moveSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, target.position);
+        if (distance >= 2)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, Time.deltaTime * moveSpeed);
+        }
+
+    }
+}
